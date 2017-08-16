898ab60606b15e9760d5d0f80652421528b5f447
tonyxie <tonyxie_0419@qq.com>
2017-08-16 13:55:34 +0800

    不是很明白，援引的意思，以后再补上

---
+++ src/Invoke.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    val invokable = Invokable()
+    invokeTwice(invokable)
+    println(invokable.numberOfInvocations)
+}
+
+class Invokable {
+    var numberOfInvocations: Int = 0
+        private set
+
+    operator fun invoke(): Invokable {
+        numberOfInvocations++
+        return this
+    }
+}
+
+fun invokeTwice(invokable: Invokable) = invokable()()
